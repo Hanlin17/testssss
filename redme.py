d7e873536a0bfb6ea168d9a4d184f5f4b6c86125
Hanlin17 <1050095627@qq.com>
2023-07-06 21:19:57 +0800

    sdadas

--- redme.py
+++ redme.py
@@ -19,3 +19,19 @@ def func2():
     if a==1:
         print(f"I am func2")
     print("dasdasd")
+
+def func3():
+    a = "dsada";
+    if a == 1:
+        print(f"I am func2")
+    print("dasdasd")
+
+    a = "sddasd";
+    if a==1:
+        print(f"I am func2")
+    print("dasdasd")
+
+    a = "sadasd";
+    if a==1:
+        print(f"I am func2")
+    print("dasdasd")
